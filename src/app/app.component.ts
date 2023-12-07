8c6b5ee7a818befaf9991d2d91f2324791f80ba8
Fernando Rojas <fernandorojasmosqueira@gmail.com>
2023-12-06 23:25:46 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from './share/top-menu/top-menu.component';
+import { AuthorComponent } from './share/author/author.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    TopMenuComponent, 
+    AuthorComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'test-websocket';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TestFormComponent } from './pages/test-form/test-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: TestFormComponent,
+        pathMatch: 'full'
+    }
+];
